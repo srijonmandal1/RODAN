50f9bfb992e820351237aa36b8aca5efbcb4a0e4
SrijonMaster1 <mandal.srijon@gmail.com>
2021-12-24 11:00:36 -0800

    add thermal camera visual display

--- Object Detection/Person Detection/RND/thermal_camera.py
+++ Object Detection/Person Detection/RND/thermal_camera.py
@@ -1,6 +1,8 @@
 import board
 import busio
 import adafruit_mlx90640
+import numpy
+import cv2
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 
@@ -12,16 +14,34 @@ print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
-while True:
-    try:
-        mlx.getFrame(frame)
-    except ValueError:
-        # these happen, no biggie - retry
-        continue
+try:
+    while True:
+        try:
+            mlx.getFrame(frame)
+        except ValueError:
+            # these happen, no biggie - retry
+            continue
 
-    for h in range(24):
-        for w in range(32):
-            t = frame[h*32 + w]
-            print("%0.1f, " % t, end="")
-        print()
-    print()
+        nested_frame = []
+
+        for h in range(24):
+            row = []
+            for w in range(32):
+                frame[h*32 + w] *= 6.3
+                row.append(frame[h*32 + w])
+            nested_frame.append(row)
+        #         t = frame[h*32 + w]
+        #         print("%0.1f, " % t, end="")
+        #     print()
+        # print()
+
+        np_array = numpy.array(nested_frame).astype('uint8')
+        grayImage = cv2.resize(cv2.cvtColor(np_array, cv2.COLOR_GRAY2BGR), dsize=(320, 240))
+        cv2.imshow("Image", grayImage)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+except KeyboardInterrupt:
+    pass
+
+cv2.destroyAllWindows()

2fcdb49bc32cf7ec21e8b3ee84c95aceb4f2497d
SrijonMaster1 <mandal.srijon@gmail.com>
2022-01-03 13:18:54 -0800

    update bluetooth client

--- bluetooth_communication_client.py
+++ bluetooth_communication_client.py
@@ -1,13 +1,15 @@
 import socket
 import json
+import time
 
 serverMACAddress = '4C:1D:96:A4:F8:7A'
 port = 5
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((serverMACAddress,port))
+s.connect((serverMACAddress, port))
 while 1:
     event = input()
     if event == "quit":
         break
-    s.send(bytes(json.dumps({"event": event, "time": time.time()}), 'UTF-8'))
+    event_json = json.dumps({"event": event, "time": time.time()})
+    s.send(bytes(event_json, 'UTF-8'))
 s.close()

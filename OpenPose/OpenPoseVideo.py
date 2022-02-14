c80b0588798064abfe0aaa200dfa441a492a8da2
SrijonMaster1 <mandal.srijon@gmail.com>
2022-02-14 11:09:25 -0800

    update

--- OpenPose/OpenPoseVideo.py
+++ OpenPose/OpenPoseVideo.py
@@ -2,24 +2,33 @@ import cv2
 import time
 import numpy as np
 import argparse
+from os.path import dirname, join
 
 parser = argparse.ArgumentParser(description='Run keypoint detection')
 parser.add_argument("--device", default="cpu", help="Device to inference on")
-parser.add_argument("--video_file", default="sample_video.mp4", help="Input Video")
+parser.add_argument("--video_file", default="vid.mp4", help="Input Video")
 
 args = parser.parse_args()
 
 MODE = "MPI"
 
 if MODE is "COCO":
-    protoFile = "pose/coco/pose_deploy_linevec.prototxt"
-    weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+    # protoFile = "pose/coco/pose_deploy_linevec.prototxt"
+    # weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+    protoFile = join(dirname(__file__),
+                    "pose/coco/pose_deploy_linevec.prototxt")
+    weightsFile = join(dirname(__file__),
+                    "pose/coco/pose_iter_440000.caffemodel")
     nPoints = 18
     POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
 
 elif MODE is "MPI" :
-    protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
-    weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+    # protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+    # weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+    protoFile = join(dirname(__file__),
+                    "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt")
+    weightsFile = join(dirname(__file__),
+                    "pose/mpi/pose_iter_160000.caffemodel")
     nPoints = 15
     POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
 
@@ -32,7 +41,9 @@ threshold = 0.1
 input_source = args.video_file
 cap = cv2.VideoCapture(input_source)
 hasFrame, frame = cap.read()
-
+print(input_source)
+# print(cap)
+# print(frame)
 vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
 
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)

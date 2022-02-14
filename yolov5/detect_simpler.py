305498219fd13fd2c359952df094aaa9cf95b46f
Kethan Vegunta <kethan@vegunta.com>
2022-02-14 12:33:54 -0800

    update

--- yolov5/detect_simpler.py
+++ yolov5/detect_simpler.py
@@ -1,13 +1,29 @@
 import torch
+import cv2
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+model = torch.hub.load('ultralytics/yolov5', 'yolov5n')  # or yolov5m, yolov5l, yolov5x, custom
 
-# Images
-img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+def get_classes_from_results(results):
+    classes_detected = {}
+    pred = results.pred[0]
 
-# Inference
-results = model(img)
+    if pred.shape[0]:
+        for c in pred[:, -1].unique():
+            n = (pred[:, -1] == c).sum()  # detections per class
+            classes_detected[results.names[int(c)]] = int(n)
 
-# Results
-results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+    return classes_detected
+
+cap = cv2.VideoCapture(0)
+for _ in range(10):
+    _, frame = cap.read()
+while True:
+    _, frame = cap.read()
+    results = model(frame)
+    print(str(get_classes_from_results(results)) + "                      ", end="\r")
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
